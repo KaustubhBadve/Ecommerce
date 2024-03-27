0c01838b846842436a97e954b8be890e4c972fd9
KaustubhBadve <kaustubhbadve7@gmail.com>
2024-03-28 00:00:08 +0530

    SSL cretificate added with domain name added

--- Client/src/Config.js/AppConfig.js
+++ Client/src/Config.js/AppConfig.js
@@ -1,2 +1,4 @@
-export const BASE_URL = "http://35.154.229.178"
+// export const BASE_URL = "http://35.154.229.178"
+
+export const BASE_URL = "https://shoppingkartapna.chickenkiller.com"
 // export const BASE_URL = "http://localhost:3033"
